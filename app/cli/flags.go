592c5ef464db167be850120942867b2fcfb7b4a1
thisisaaronland <devnull@localhost>
2024-02-03 18:32:56 -0800

    snapshot: almost working get descendants

--- app/cli/flags.go
+++ app/cli/flags.go
@@ -11,6 +11,8 @@ var command string
 
 var id int64
 
+var per_page int64
+
 func DefaultFlagSet() *flag.FlagSet {
 
 	fs := flagset.NewFlagSet("descendants")
@@ -18,6 +20,8 @@ func DefaultFlagSet() *flag.FlagSet {
 
 	fs.StringVar(&command, "command", "", "...")
 	fs.Int64Var(&id, "id", 0, "...")
+
+	fs.Int64Var(&per_page, "per-page", 10, "...")	
 
 	return fs
 }
--- app/cli/get_descendants.go
+++ app/cli/get_descendants.go
@@ -2,15 +2,31 @@ package cli
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
-	spelunker "github.com/whosonfirst/go-whosonfirst-spelunker"
+	"github.com/aaronland/go-pagination/countable"
+	"github.com/whosonfirst/go-whosonfirst-spelunker"
 )
 
 func get_descendants(ctx context.Context, sp spelunker.Spelunker) error {
 
-	_, _, err := sp.GetDescendants(ctx, id, nil)
+	// Eventually we'll need to check if we're doing cursor-base pagination
 
-	slog.Info("D", "error", err)
+	pg_opts, err := countable.NewCountableOptions()
+
+	if err != nil {
+		return fmt.Errorf("Failed to create countable options, %w", err)
+	}
+
+	pg_opts.PerPage(per_page)
+
+	r, _, err := sp.GetDescendants(ctx, id, pg_opts)
+
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve descendants, %w", err)
+	}
+
+	slog.Info("OK", "count", len(r))
 	return nil
 }
